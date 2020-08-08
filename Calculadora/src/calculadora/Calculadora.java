2eb93d200d7e2da33eac01b9cd2d8f165b5098b4
Jackson Callado <34450896+LyTT19@users.noreply.github.com>
2020-08-07 22:33:53 -0300

    Atividade 1
    Calculadora desenvolvida conforme solicitado durante a atividade

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -5,17 +5,99 @@
  */
 package calculadora;
 
-/**
- *
- * @author lytt1
- */
-public class Calculadora {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+public class Calculadora {
+    
+    public static double soma(double a, double b){
+        double res = a + b;
+        return res;
     }
     
+    public static double subtracao(double a, double b){
+        double res = a - b;
+        return res;
+    }
+    
+    public static double multiplicacao(double a, double b){
+        double res = a * b;
+        return res;
+    }    
+    
+    public static double divisao(double a, double b){
+        double res = a / b;
+        return res;
+    }
+    
+    public static double quadrado(double a){
+        double res = Math.pow(a, 2);
+        return res;
+    }    
+    
+    public static void calculo(){
+        
+        Scanner teclado = new Scanner(System.in);
+        double resultado = 0;
+        
+        System.out.println("+: Soma");
+        System.out.println("-: Subtração");
+        System.out.println("*: Multiplicação");
+        System.out.println("/: Divisão:");
+        System.out.println("^: Quadrado");
+        System.out.println("Digite uma operação: ");
+        String op = teclado.nextLine();
+               
+        if(op.equals("+")){
+            System.out.println("Digite um numero: ");
+            double x = teclado.nextFloat();
+            System.out.println("Digite outro numero");
+            double y = teclado.nextFloat();
+            resultado = soma(x, y);
+            System.out.println(x + " + " + y + " = " + resultado );
+        }
+        if(op.equals("-")){
+            System.out.println("Digite um numero: ");
+            double x = teclado.nextFloat();
+            System.out.println("Digite outro numero");
+            double y = teclado.nextFloat();
+            resultado = subtracao(x, y);
+            System.out.println(x + " - " + y + " = " + resultado );
+        }
+        if(op.equals("*")){
+            System.out.println("Digite um numero: ");
+            double x = teclado.nextFloat();
+            System.out.println("Digite outro numero");
+            double y = teclado.nextFloat();
+            resultado = multiplicacao(x, y);
+            System.out.println(x + " * " + y + " = " + resultado );
+        }
+        if(op.equals("/")){
+            System.out.println("Digite um numero: ");
+            double x = teclado.nextFloat();
+            System.out.println("Digite outro numero");
+            double y = teclado.nextFloat();
+            resultado = divisao(x, y);
+            System.out.println(x + " / " + y + " = " + resultado );
+        }
+        if(op.equals("^")){
+            System.out.println("Digite um numero: ");
+            double x = teclado.nextFloat();
+            resultado = quadrado(x);
+            System.out.println(x + " ^ 2 = " + resultado );
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        String resposta = "";
+        
+        do{
+            calculo();
+            System.out.println("Deseja realizar outro calculo? (S/N)");
+            resposta = teclado.nextLine().toLowerCase();
+        }while(resposta.equals("s"));        
+        
+    }
 }
