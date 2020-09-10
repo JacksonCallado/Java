49e55bf1864d30f05c6a4306dfa5a4bb6eb71e0a
Jackson Callado <34450896+LyTT19@users.noreply.github.com>
2020-09-10 00:56:05 -0300

    ajustes

--- sintaxe-basica/src/Programa.java
+++ sintaxe-basica/src/Programa.java
@@ -2,7 +2,10 @@
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo!");
-	}
-	
+		int fatorial = 1;
+        for (int i = 1; i < 11; i++) {
+            fatorial *= i;
+            System.out.println("Fatorial de " + i + " = " +fatorial);
+        }		
+	}	
 }
